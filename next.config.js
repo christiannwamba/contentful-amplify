fd272432706a16550bcbfb7e784214a19b4fa6c7
christiannwamba <nwambachristian@gmail.com>
2023-01-31 12:17:50 +0000

    Add next-transpile-module

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')(['react-syntax-highlighter'])
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);
